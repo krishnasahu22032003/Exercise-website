035d7d83332d611f325fe1faf737eb4f2a6f0bea
Krishna Sahu <krishna.sahu.work@gmail.com>
2025-07-04 13:27:03 +0530

    HorizintalScrollBar component created

--- src/Components/HorizontalScrollbar.jsx
+++ src/Components/HorizontalScrollbar.jsx
@@ -1,12 +1,56 @@
-import { useContext } from "react"
-import { ScrollMenu,VisibilityContext } from "react-horizontal-scrolling-menu"
-import { Box,Typography } from "@mui/material"
-const HorizontalScrollbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import React, { useContext } from 'react';
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+import { Box, Typography } from '@mui/material';
 
-export default HorizontalScrollbar
+import ExerciseCard from './ExerciseCard';
+import BodyPart from './Bodypart';
+import RightArrowIcon from '../assets/icons/right-arrow.png';
+
+
+const LeftArrow = () => {
+  const { scrollPrev } = useContext(VisibilityContext);
+
+  return (
+    <Typography
+      onClick={() => scrollPrev()}
+      className="right-arrow cursor-pointer flex items-center justify-center w-10 h-10 rounded-full hover:bg-gray-200 transition"
+    >
+      <img src='/images/left-arrow.png' alt="right-arrow" className="w-6 h-6" />
+    </Typography>
+  );
+};
+
+const RightArrow = () => {
+  const { scrollNext } = useContext(VisibilityContext);
+
+  return (
+    <Typography
+      onClick={() => scrollNext()}
+      className="left-arrow cursor-pointer flex items-center justify-center w-10 h-10 rounded-full hover:bg-gray-200 transition"
+    >
+      <img src='/images/right-arrow.png' alt="right-arrow" className="w-6 h-6" />
+    </Typography>
+  );
+};
+
+const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => (
+  <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} className="overflow-hidden">
+    {data.map((item) => (
+      <Box
+        key={item.id || item}
+        itemId={item.id || item}
+        title={item.id || item}
+        m="0 40px"
+        className="inline-block"
+      >
+        {bodyParts ? (
+          <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} />
+        ) : (
+          <ExerciseCard exercise={item} />
+        )}
+      </Box>
+    ))}
+  </ScrollMenu>
+);
+
+export default HorizontalScrollbar;
