3e7e2239b4cd8a673adeca35e2acbc89b267b435
Krishna Sahu <krishna.sahu.work@gmail.com>
2025-07-07 10:22:30 +0530

    Navbar updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,39 +1,37 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import { Stack } from '@mui/material';
-
 
 const Navbar = () => (
   <Stack
     direction="row"
-    className="flex-wrap px-5 mt-[20px] sm:mt-[32px] gap-[40px] sm:gap-[123px] justify-baseline"
+    alignItems="center"
+    className="w-full max-w-[1440px] mx-auto px-6 sm:px-6 mt-[20px] sm:mt-[32px]"
   >
-    <Link to="/">
-      <img
-        src='/images/Logo.png'
-        alt="logo"
-        className="w-[48px] h-[48px] mx-[20px]"
-      />
-    </Link>
+    {/* Logo + Nav Links together */}
+    <div className="flex flex-wrap items-center gap-5 sm:gap-10 text-[18px] sm:text-[24px] font-[Alegreya] text-[#2D0C57]">
+      {/* Logo */}
+      <Link to="/">
+        <img
+          src="/images/Logo.png"
+          alt="logo"
+          className="w-[64px] h-[64px] sm:w-[88px] sm:h-[88px] object-contain"
+        />
+      </Link>
 
-    <Stack
-      direction="row"
-      alignItems="flex-end"
-      className="gap-[40px] text-[24px] font-[Alegreya]"
-    >
+      {/* Nav Links */}
       <Link
         to="/"
-        className="no-underline text-[#3A1212] border-b-[3px] border-[#FF2625]"
+        className="no-underline text-transparent bg-clip-text bg-gradient-to-r from-[#FF4D4D] to-[#1B1B1B] transition duration-200"
       >
         Home
       </Link>
       <a
         href="#exercises"
-        className="no-underline text-[#3A1212]"
+        className="no-underline transition duration-200 hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-[#FF4D4D] hover:to-[#1B1B1B]"
       >
         Exercises
       </a>
-    </Stack>
+    </div>
   </Stack>
 );
 
