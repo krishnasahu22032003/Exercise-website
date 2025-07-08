b49cd0c41d52acc7dc848382689620d3be2ddc02
Krishna Sahu <krishna.sahu.work@gmail.com>
2025-07-09 00:27:39 +0530

    Deployment bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import Navbar from "./Components/Navbar";
 import HeroBanner from "./Components/HeroBanner";
 import Features from "./Components/Features";
@@ -7,25 +6,8 @@ import Membership from "./Components/Membership";
 import FAQS from "./Components/FAQS";
 import CTA from "./Components/CTA";
 import Stats from "./Components/Stats";
-import Footer from "./Components/Footer";
-import Loader from "./Components/Loader";
-
+import Footer from "./Components/Footer"
 function App() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // Remove delay, load immediately after content is ready
-    setLoading(false);
-  }, []);
-
-  if (loading) {
-    return (
-      <div className="w-screen h-screen flex items-center justify-center">
-        <Loader />
-      </div>
-    );
-  }
-
   return (
     <div className="w-full h-auto overflow-hidden">
       <Navbar />
